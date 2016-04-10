fa4f05c34df08c1625c09c1bd2342a7913820889
eisbaer66 <janweidner87@gmail.com>
2016-04-10 17:33:33 +0200

    logging

--- TF2Items.Ui/App.xaml.cs
+++ TF2Items.Ui/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
+using log4net;
 using TF2Items.Ui.Properties;
 
 namespace TF2Items.Ui
@@ -14,6 +16,19 @@ namespace TF2Items.Ui
     /// </summary>
     public partial class App : Application
     {
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            base.OnStartup(e);
+        }
+
+        private void ApplicationOnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs args)
+        {
+            Log.Fatal("DispatcherUnhandledException", args.Exception);
+        }
+
         private void OnExit(object sender, ExitEventArgs e)
         {
             Settings.Default.Save();
