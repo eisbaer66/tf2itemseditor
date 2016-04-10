c675e3fdf5c0d5dfd6be90fef670f25491240b6b
eisbaer66 <janweidner87@gmail.com>
2016-04-10 14:27:01 +0200

    PushTo-GitHub now takes ReleaseName as parameter;

--- scripts/PushTo-GitHub.ps1
+++ scripts/PushTo-GitHub.ps1
@@ -1,16 +1,16 @@
 ﻿param(
-[string]$GitHubToken)
+[string]$GitHubToken,
+[string]$ReleaseName)
 
 $ErrorActionPreference = "Stop"
 
-$releaseName = $Env:RELEASE_RELEASENAME
 $zipLocation = "$Env:SYSTEM_DEFAULTWORKINGDIRECTORY/tf2itemseditor/zip/TF2ItemsEditor.zip"
 
 $authheader = "Basic " + ([Convert]::ToBase64String([System.Text.encoding]::ASCII.GetBytes($GitHubToken)))
 $header = @{Authorization=$authheader}
 
 
-$body = "{""tag_name"": ""$releaseName"",""target_commitish"": ""eisbaer"",""name"": ""$releaseName"",""body"": ""test"",""draft"": true,""prerelease"": true}"
+$body = "{""tag_name"": ""$ReleaseName"",""target_commitish"": ""eisbaer"",""name"": ""$ReleaseName"",""body"": """",""draft"": true,""prerelease"": true}"
 
 $response = Invoke-RestMethod -Method Post -Uri https://api.github.com/repos/eisbaer66/tf2itemseditor/releases -Body $body -Headers $header;
 $uploadUrl = $response.upload_url
